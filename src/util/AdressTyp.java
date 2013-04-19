5377f3affab4e8de721e0b0440a141054ec0a194
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-19 15:50:49 +0200

    AdressTyp implementiert.

--- src/util/AdressTyp.java
+++ src/util/AdressTyp.java
@@ -1,5 +1,33 @@
 package util;
 
 public class AdressTyp {
+	
+	private String strasse;
+	private int hausnummer;
+	private int postleitzahl;
+	private String ortsname;
+	
+	public AdressTyp(String strasse, int hausnummer, int postleitzahl, String ortsname) {
+		this.strasse = strasse;
+		this.hausnummer = hausnummer;
+		this.postleitzahl = postleitzahl;
+		this.ortsname = ortsname;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public int getHausnummer() {
+		return hausnummer;
+	}
+
+	public int getPostleitzahl() {
+		return postleitzahl;
+	}
+
+	public String getOrtsname() {
+		return ortsname;
+	}
 
 }
