9e1733fec8325d3d66b2a35c7a725d4231c93d24
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-19 14:59:42 +0200

    Testdummies hinzugefuegt

---
+++ src/test/system/Systemtest.java
@@ -0,0 +1,14 @@
+package test.system;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Systemtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
