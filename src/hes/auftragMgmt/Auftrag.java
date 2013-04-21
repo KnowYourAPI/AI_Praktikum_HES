e7c03944b1051eecdf793325c5749dc75cc7b10a
Daniela Barths <daniela.barths@googlemail.com>
2013-04-21 16:55:50 +0200

    Many-to-Many mit Produkt eingefuegt

--- src/hes/auftragMgmt/Auftrag.java
+++ src/hes/auftragMgmt/Auftrag.java
@@ -1,12 +1,17 @@
 package hes.auftragMgmt;
 
+import java.util.List;
+
 import hes.kundeMgmt.Kunde;
+import hes.produktMgmt.Produkt;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.TableGenerator;
 
@@ -22,4 +27,8 @@ public class Auftrag {
 	@JoinColumn(name="kunde_id")
 	private Kunde kunde;
 
+	@ManyToMany
+	@JoinTable(name="Join_Produkt_Auftrag", joinColumns={@JoinColumn(name="auftragId")},
+	inverseJoinColumns={@JoinColumn(name="produktId")})
+	private List<Produkt> produkte;
 }
