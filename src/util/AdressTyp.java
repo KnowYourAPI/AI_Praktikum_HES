b466dc6beca9b3778e9c22c8ac69a493508dcf5b
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-21 14:51:26 +0200

    AdressTyp angelegt, der immutable ist und Adresse eingefuehrt fuer Verwendung in KundeMgmt

--- src/util/AdressTyp.java
+++ src/util/AdressTyp.java
@@ -1,49 +1,31 @@
 package util;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.TableGenerator;
-
-@Entity
 public class AdressTyp {
 	
-	@Id
-	@TableGenerator(name="adressTypId", table="adressTypPrimaryKeyTable", pkColumnName="adressTypPrimaryKey", pkColumnValue="nextAdressTypKey", allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.TABLE, generator="adressTypId")
-	private int adressTypId;
-	@Column(nullable=false)
-	private String strasse;
-	@Column(nullable=false)
-	private String hausnummer;
-	@Column(nullable=false)
-	private String postleitzahl;
-	@Column(nullable=false)
-	private String ortsname;
+	private Adresse adresse;
 	
-	public AdressTyp(String strasse, String hausnummer, String postleitzahl, String ortsname) {
-		this.strasse = strasse;
-		this.hausnummer = hausnummer;
-		this.postleitzahl = postleitzahl;
-		this.ortsname = ortsname;
+	public AdressTyp(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getAdressTypId() {
+		return adresse.getAdressTypId();
 	}
 
 	public String getStrasse() {
-		return strasse;
+		return adresse.getStrasse();
 	}
 
 	public String getHausnummer() {
-		return hausnummer;
+		return adresse.getHausnummer();
 	}
 
 	public String getPostleitzahl() {
-		return postleitzahl;
+		return adresse.getPostleitzahl();
 	}
 
 	public String getOrtsname() {
-		return ortsname;
+		return adresse.getOrtsname();
 	}
 
 }
---
+++ src/util/Adresse.java
@@ -0,0 +1,73 @@
+package util;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.TableGenerator;
+
+@Entity
+public class Adresse {
+	
+	@Id
+	@TableGenerator(name="adressTypId", table="adressTypPrimaryKeyTable", pkColumnName="adressTypPrimaryKey", pkColumnValue="nextAdressTypKey", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.TABLE, generator="adressTypId")
+	private int adressTypId;
+	@Column(nullable=false)
+	private String strasse;
+	@Column(nullable=false)
+	private String hausnummer;
+	@Column(nullable=false)
+	private String postleitzahl;
+	@Column(nullable=false)
+	private String ortsname;
+	
+	public Adresse(String strasse, String hausnummer, String postleitzahl, String ortsname) {
+		this.strasse = strasse;
+		this.hausnummer = hausnummer;
+		this.postleitzahl = postleitzahl;
+		this.ortsname = ortsname;
+	}
+
+	public int getAdressTypId() {
+		return adressTypId;
+	}
+
+	public void setAdressTypId(int adressTypId) {
+		this.adressTypId = adressTypId;
+	}
+
+	public String getStrasse() {
+		return strasse;
+	}
+
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+
+	public String getHausnummer() {
+		return hausnummer;
+	}
+
+	public void setHausnummer(String hausnummer) {
+		this.hausnummer = hausnummer;
+	}
+
+	public String getPostleitzahl() {
+		return postleitzahl;
+	}
+
+	public void setPostleitzahl(String postleitzahl) {
+		this.postleitzahl = postleitzahl;
+	}
+
+	public String getOrtsname() {
+		return ortsname;
+	}
+
+	public void setOrtsname(String ortsname) {
+		this.ortsname = ortsname;
+	}
+
+}
