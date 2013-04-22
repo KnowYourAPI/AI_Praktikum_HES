1d87329ce086507c4b2842c6a4f57dfff01e05b8
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-22 21:42:28 +0200

    Annotations fuer Rechnung und Zahlungseingang hinzugefuegt, damit diese korrekt persistiert werden koennen

--- src/hes/rechnungMgmt/Rechnung.java
+++ src/hes/rechnungMgmt/Rechnung.java
@@ -3,10 +3,13 @@ package hes.rechnungMgmt;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.TableGenerator;
 
 @Entity
@@ -16,8 +19,11 @@ public class Rechnung {
 	@TableGenerator(name="rechnungId", table="rechnungPrimaryKeyTable", pkColumnName="rechnungPrimaryKey", pkColumnValue="nextRechnungKey", allocationSize=1)
 	@GeneratedValue(strategy=GenerationType.TABLE, generator="rechnungId")
 	private int rechnungId;
+	@Column(nullable=false)
 	private Date rechnungsDatum;
+	@Column(nullable=false)
 	private boolean istBezahlt;
+	@OneToMany(targetEntity=Zahlungseingang.class, mappedBy="rechnung", cascade=CascadeType.ALL)
 	private List<Zahlungseingang> zahlungseingaenge;
 
 }
--- src/hes/rechnungMgmt/Zahlungseingang.java
+++ src/hes/rechnungMgmt/Zahlungseingang.java
@@ -1,9 +1,12 @@
 package hes.rechnungMgmt;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.TableGenerator;
 
 @Entity
@@ -13,7 +16,10 @@ public class Zahlungseingang {
 	@TableGenerator(name="zahlungseingangId", table="zahlungseingangPrimaryKeyTable", pkColumnName="zahlungseingangPrimaryKey", pkColumnValue="nextZahlungseingangKey", allocationSize=1)
 	@GeneratedValue(strategy=GenerationType.TABLE, generator="zahlungseingangId")
 	private int zahlungseingangId;
+	@ManyToOne
+	@JoinColumn(name="rechnung_id")
 	private Rechnung rechnung;
+	@Column(nullable=false)
 	private float betrag;
 
 }
