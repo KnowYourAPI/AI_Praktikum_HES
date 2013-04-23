72d1b99a20cd7e9b727e49d216a4842615a5d299
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-23 22:54:50 +0200

    Transaktionshandling aus dem Lieferungrepository entfernt

--- src/hes/lieferungMgmt/LieferungRepository.java
+++ src/hes/lieferungMgmt/LieferungRepository.java
@@ -5,22 +5,15 @@ import org.hibernate.Session;
 class LieferungRepository {
 	
 	Lieferung ladeLieferung(int lieferungId, Session session) {
-		session.beginTransaction();
 		Lieferung lieferung = (Lieferung) session.get(Lieferung.class, lieferungId);
-		session.getTransaction().commit();
-		
 		return lieferung;
 	}
 	
 	void speichereLieferung(Lieferung lieferung, Session session) {
-		session.beginTransaction();
 		session.save(lieferung);
-		session.getTransaction().commit();
 	}
 	
 	void aktualisiereLieferung(Lieferung lieferung, Session session) {
-		session.beginTransaction();
 		session.update(lieferung);
-		session.getTransaction().commit();
 	}
 }
