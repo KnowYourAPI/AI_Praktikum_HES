c8707b77c2bcf11a804d71dca282fabab3771d38
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-23 22:57:54 +0200

    Transaktionshandling aus dem RechnungRepository entfernt

--- src/hes/rechnungMgmt/RechnungRepository.java
+++ src/hes/rechnungMgmt/RechnungRepository.java
@@ -8,17 +8,13 @@ import org.hibernate.Session;
 public class RechnungRepository {
 	
 	public void speichereRechnung(Rechnung rechnung, Session session) {
-		session.beginTransaction();
 		session.save(rechnung);
-		session.getTransaction().commit();
 	}
 	
 	public Rechnung ladeRechnung(int rechnungId, Session session) {
-		session.beginTransaction();
 		Query query = session.createQuery("from Rechnung where rechnungId = :rechnungId");
 		query.setParameter("rechnungId", rechnungId);
 		List<?> queryResult = query.list();
-		session.getTransaction().commit();
 		
 		if(!queryResult.isEmpty()) {
 			Rechnung rechnung = (Rechnung)queryResult.get(0);
