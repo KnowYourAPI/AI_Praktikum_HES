1095e89c5459711f83d8c4647f4401fa05826697
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-23 14:15:52 +0200

    Eager-loading fuer die zugehoerigen Zahlungseingaenge der Rechnungen hinzugefuegt

--- src/hes/rechnungMgmt/Rechnung.java
+++ src/hes/rechnungMgmt/Rechnung.java
@@ -9,6 +9,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,7 @@ public class Rechnung {
 	private Date rechnungsDatum;
 	@Column(nullable=false)
 	private boolean istBezahlt;
-	@OneToMany(targetEntity=Zahlungseingang.class, mappedBy="rechnung", cascade=CascadeType.ALL)
+	@OneToMany(targetEntity=Zahlungseingang.class, mappedBy="rechnung", cascade=CascadeType.ALL, fetch=FetchType.EAGER)
 	private List<Zahlungseingang> zahlungseingaenge;
 	@OneToOne
 	private Auftrag auftrag;
