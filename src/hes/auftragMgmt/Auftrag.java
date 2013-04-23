2af22f0b2fe4ef7ed42f86b798fb8fc74b07a744
KnowYourAPI <addictedtoicetea@gmail.com>
2013-04-23 09:58:08 +0200

    Auftrag-Klasse implementiert

--- src/hes/auftragMgmt/Auftrag.java
+++ src/hes/auftragMgmt/Auftrag.java
@@ -1,16 +1,13 @@
 package hes.auftragMgmt;
 
-import java.util.List;
+import java.util.Date;
 
-import hes.produktMgmt.Produkt;
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.TableGenerator;
 
 @Entity
@@ -20,22 +17,54 @@ public class Auftrag {
 	@TableGenerator(name="auftragid", table="auftragPrimaryKeyTable", pkColumnName="auftragPrimaryKey", pkColumnValue="nextAuftragKey", allocationSize=1)
 	@GeneratedValue(strategy=GenerationType.TABLE, generator="auftragid")
 	private int auftragId;
+	@Column(nullable=false)
+	private boolean istAbgeschlossen;
+	@Column(nullable=false)
+	private Date beauftragtAm;
+	@OneToOne
+	private Angebot angebot;
 	
 	public Auftrag() {}
 	
 	public Auftrag(Angebot angebot) {
-		
+		this.istAbgeschlossen = false;
+		this.beauftragtAm = new Date();
+		this.angebot = angebot;
 	}
-	//TODO: Benoetigt in RechnungMgmt
+
 	public float getGesamtpreis() {
-		return 0;
+		return angebot.getGesamtPreis();
 	}
-	
+
 	public int getAuftragId() {
 		return auftragId;
 	}
 
 	public void setAuftragId(int auftragId) {
 		this.auftragId = auftragId;
+	}
+
+	public boolean isIstAbgeschlossen() {
+		return istAbgeschlossen;
+	}
+
+	public void setIstAbgeschlossen(boolean istAbgeschlossen) {
+		this.istAbgeschlossen = istAbgeschlossen;
+	}
+
+	public Date getBeauftragtAm() {
+		return beauftragtAm;
+	}
+
+	public void setBeauftragtAm(Date beauftragtAm) {
+		this.beauftragtAm = beauftragtAm;
+	}
+
+	public Angebot getAngebot() {
+		return angebot;
+	}
+
+	public void setAngebot(Angebot angebot) {
+		this.angebot = angebot;
 	}
 }
