9e1997dc2460c349151fc3796bfc4246a1113d37
Daniela Barths <daniela.barths@googlemail.com>
2013-04-23 18:32:30 +0200

    Kommentare eingefuegt

--- src/hes/auftragMgmt/IAuftragMgmt.java
+++ src/hes/auftragMgmt/IAuftragMgmt.java
@@ -5,45 +5,53 @@ import hes.produktMgmt.Produkt;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
 public interface IAuftragMgmt {
 
 	/**
 	 * @param kunde Der Kunde, fuer den das Angebot erstellt werden soll.
+	 * @param session Die aktuelle Session.
 	 * @return Das erstellte Angebot.
 	 * */
-	Angebot erstelleAngebot(Kunde kunde);
+	Angebot erstelleAngebot(Kunde kunde, Session session);
 	
 	/**
 	 * @param angebot Das Angebot, zu dem das Produkt hinzugefuegt werden soll.
 	 * @param produkt Das dem Angebot hinzuzufuegende Produkt.  
 	 * @param menge Die gewuenschte Menge des hinzuzufuegenden Produkts.
+	 * @param session Die aktuelle Session.
 	 * @return Das Angebot, dem das Prokukt in entspechender Menge hinzugefuegt wurde.
 	 * */
-	Angebot fuegeProduktZuAngebotHinzu(Angebot angebot, Produkt produkt, int menge);
+	Angebot fuegeProduktZuAngebotHinzu(Angebot angebot, Produkt produkt, int menge, Session session);
 	
 	/**
 	 * @param angebot Das Angebot, aus das Produkt entfernt werden soll.
 	 * @param produkt Das aus dem Angebot zu entfernende Produkt.
+	 * @param session Die aktuelle Session.
  	 * @return Das Angebot, aus dem das Produkt entfernt wurde.
 	 * */
-	Angebot entferneProduktAusAngebot(Angebot angebot, Produkt produkt);
+	Angebot entferneProduktAusAngebot(Angebot angebot, Produkt produkt, Session session);
 	
 	/**
 	 * @param angebot Das Angebot, aus dem ein Auftrag erstellt wird.
+	 * @param session Die aktuelle Session.
 	 * @return Der erstellte Auftrag. 
 	 * */
-	Auftrag erstelleAuftrag(Angebot angebot);
+	Auftrag erstelleAuftrag(Angebot angebot, Session session);
 	
 	/**
 	 * @param produkt Das Produkt, das in den Auftraegen vorhanden sein soll.
+	 * @param session Die aktuelle Session.
 	 * @return Eine Liste aller nicht abgeschlossenen Auftraege im System, die das Produkt enthalten.
 	 * */
-	List<Auftrag> getNichtAbgeschlosseneAuftraege(Produkt produkt);
+	List<Auftrag> getNichtAbgeschlosseneAuftraege(Produkt produkt, Session session);
 	
 	/**
+	 * @param session Die aktuelle Session.
 	 * @param auftragId Die ID des als abgeschlossen zu markierenden Auftrags.
 	 * */
-	void markiereAuftragAlsAbgeschlossen(int auftragId);
+	void markiereAuftragAlsAbgeschlossen(int auftragId, Session session);
 	
 	/**
 	 * @param angebot Das Angebot, von dem ein AngebotsTyp-Objekt erstellt werden soll.
@@ -56,5 +64,19 @@ public interface IAuftragMgmt {
 	 * @return Das AuftragsTyp-Objekt, zu dem uebergebenen Auftrag.
 	 * */
 	AuftragTyp getAuftragTyp(Auftrag auftrag);
+	
+	/**
+	 * @param angebotId Die ID des gewuenschten Angebots.
+	 * @param session Die aktuelle Session.
+	 * @return Das Angebot mit der uebergebenen ID.
+	 * */
+	Angebot getAngebot(int angebotId, Session session);
+	
+	/**
+	 * @param auftragId Die ID des gewuenschten Auftrags.
+	 * @param session Die aktuelle Session.
+	 * @return Der Auftrag, der zur uebergebenen ID gehoert.
+	 * */
+	Auftrag auftrag(int AuftragId, Session session);
 
 }
