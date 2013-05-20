47ba488955bb9cefe2094e45bbe2b3011d0850fc
KnowYourAPI <addictedtoicetea@gmail.com>
2013-05-20 16:29:03 +0200

    Hinzugefuegt, dass sich das Redundanzmgmt auch beim Nameservice anmeldet.

--- src/hes/redundanzMgmt/RedundanzMgmtFassade.java
+++ src/hes/redundanzMgmt/RedundanzMgmtFassade.java
@@ -1,16 +1,28 @@
 package hes.redundanzMgmt;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 
 public class RedundanzMgmtFassade implements IRedundanzMgmt {
 	
 	private Dispatcher dispatcher;
 	private Monitor monitor;
+	private String name;
 	
-	public RedundanzMgmtFassade() {
+	public RedundanzMgmtFassade(String redundanzMgmtName) {
 		this.dispatcher = new Dispatcher();
 		this.monitor = new Monitor();
 		monitor.addObserver(dispatcher);
+		this.name = redundanzMgmtName;
+		
+		try {
+			Naming.rebind(name, this);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
